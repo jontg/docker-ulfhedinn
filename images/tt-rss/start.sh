fa5fd80555041e87ff73dd07f7f5a766abbb17ea
Jon Tomas Gretarsson <jon.gretarsson@gmail.com>
2014-01-20 23:39:18 -0800

    Start FastCGI and create the mysql database for TT-RSS if it is not available

--- images/tt-rss/start.sh
+++ images/tt-rss/start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# I don't know how to start this silly daemon from supervisord
+service php5-fpm start
+
+# Give MySQL a few seconds to start up
 sleep 5
+
+if [ ! mysql -e 'use ttrss' ]; then
+  mysqladmin create ttrss
+fi

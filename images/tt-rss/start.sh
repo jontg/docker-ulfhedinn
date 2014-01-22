006d0b815ebe07e54caebdce7821b82c04cf734d
Jon Tomas Gretarsson <jon.gretarsson@gmail.com>
2014-01-22 08:19:04 -0800

    Restore-on-boot if an environment variable is set

--- images/tt-rss/start.sh
+++ images/tt-rss/start.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 # I don't know how to start this silly daemon from supervisord
-/usr/sbin/service php5-fpm start
+service php5-fpm start
 
 # Give MySQL a few seconds to start up
-/bin/sleep 5
+sleep 5
 
-if ! /usr/bin/mysql -u root -ptt-rss -e 'use ttrss'; then
-  /usr/bin/mysqladmin create ttrss
-  /usr/bin/mysqladmin -u root password 'tt-rss'
+if ! mysql -u root -ptt-rss -e 'use tt_rss'; then
+  echo "World appears to be uninitialized; creating table and setting the password"
+  mysqladmin -u root password 'tt-rss'
+  mysqladmin -u root -ptt-rss create tt_rss
 
   chmod -R 777 /var/www/tt-rss/{cache/images,cache/upload,cache/export,cache/js,feed-icons,lock}
 fi
+
+if [ "$(ls -A /backups)" ]; then
+  if [ ! -z $DO_RESTORE ]; then
+    FILE=$(ls -t /backups | head -n1)
+    echo "Restoring from /backups/$FILE"
+    mysql -u root -ptt-rss tt_rss < /backups/$FILE
+  fi
+fi
+
+echo DONE

4eeef9a4acf6e293fcc6a33cda2ef638255aaeac
Jon Tomas Gretarsson <jon.gretarsson@gmail.com>
2014-02-16 18:12:45 -0800

    Bug fixes

--- bin/run.sh
+++ bin/run.sh
@@ -5,6 +5,5 @@ mkdir -p $BASE_DIR/tt-rss/{logs,backups}
 docker run -d \
     -v $BASE_DIR/tt-rss/logs:/logs \
     -v $BASE_DIR/tt-rss/backups:/backups \
-    -p 2222:22 \
-    -p 80:80 \
+    -P \
     rpi-tt-rss:latest

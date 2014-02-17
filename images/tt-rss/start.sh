989a978a858ebf60c180f932231fbf754a2de4ad
Jon Tomas Gretarsson <jon.gretarsson@gmail.com>
2014-02-16 17:18:17 -0800

    Miscellaneous corrections

--- images/tt-rss/start.sh
+++ images/tt-rss/start.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-# I don't know how to start this silly daemon from supervisord
-service php5-fpm start
-
 # Give MySQL a few seconds to start up
 sleep 5
 
-if ! mysql -u root -ptt-rss -e 'use tt_rss'; then
+if mysql -u root -ptt-rss -e 'use tt_rss'; then
   echo "World appears to be uninitialized; creating table and setting the password"
   mysqladmin -u root password 'tt-rss'
   mysqladmin -u root -ptt-rss create tt_rss
-
-  chmod -R 777 /var/www/tt-rss/{cache/images,cache/upload,cache/export,cache/js,feed-icons,lock}
 fi
 
-if [ "$(ls -A /backups)" ]; then
+if [ ! -z $START_FROM_BACKUP ]; then
   if [ ! -z $DO_RESTORE ]; then
     FILE=$(ls -t /backups | head -n1)
-    echo "Restoring from /backups/$FILE"
-    mysql -u root -ptt-rss tt_rss < /backups/$FILE
+    if [ -z $FILE ]; then
+      echo "No file found!"
+    else
+      echo "Restoring from /backups/$FILE"
+      mysql -u root -ptt-rss tt_rss < /backups/$FILE
+    fi
   fi
 fi
+
+chmod -R 777 /var/www/tt-rss/{cache/images,cache/upload,cache/export,cache/js,feed-icons,lock}
 
 echo DONE
